bcf0f46b25d0c58af18d70ccdd90d0a47e1b146b
unknown <mirymiroslavy@gmail.com>
2026-02-28 17:59:15 +0300

    fix start

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+
+def get_mask_card_number(card_num: Union[str, int]) -> Union[str]:
+    '''Функция принимающая на вход номер карты'''
+    card_str = str(card_num)
+    if len(card_str) == 16:
+        mask = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
+        return mask
+    else:
+        return "Ошибка, проверьте правильность ввода номера карты"
+# возвращает замаскированый номер карты
+
+
+def get_mask_account(bank_num: int | str) -> str:
+    '''Функция принимающая на вход номер счёта'''
+    bank_str = str(bank_num)
+    if len(bank_str) != 6:
+        masked = f"**{bank_str[-4:]}"
+        return masked
+# возвращает замаскированный банковский счёт
