ab613db32607f089987f969cae99af5cd1f56c0e
unknown <mirymiroslavy@gmail.com>
2026-02-28 19:14:50 +0300

    init

--- src/widget.py
+++ src/widget.py
@@ -2,14 +2,21 @@ from masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(some_info: str) -> str:
-    '''  Принимает на вход номер счёта или карты, маскирует в зависимости от входных данных '''
+    """Принимает на вход номер счёта или карты, маскирует в зависимости от входных данных"""
     part_name = some_info.split()
     number = part_name[-1]
-    together = ' '.join(part_name[:-1])
-    if together.upper() == 'Счет':
+    together = " ".join(part_name[:-1])
+    if together.upper() == "Счет":
         masked_number = get_mask_account(number)
     else:
         masked_number = get_mask_card_number(number)
+    return f"{together}{masked_number}"
 
 
-
+def get_date(date_not_revite: str) -> str:
+    """Принимает на вход дату формата "2024-03-11Т02:26:18.671407" и возвращает в формате
+    "ДД.ММ.ГГГГ" ("11.03.2024")."""
+    day = date_not_revite[8:10]
+    month = date_not_revite[5:7]
+    year = date_not_revite[:4]
+    return f"{day}.{month}.{year}"
