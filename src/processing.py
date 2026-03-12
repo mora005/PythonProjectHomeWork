ea4cb4dc30fa31d413e5375755a391d6932d14d0
unknown <mirymiroslavy@gmail.com>
2026-03-12 22:02:57 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -12,7 +12,15 @@ def filter_by_state(user_information: List[Dict], state: str = "EXECUTED") -> Li
 
     return filter_dicts
 
-#list_dicts = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+
+# list_dicts = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 # {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 # {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 # {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+def sort_by_date(user_dict: List[Dict[str, Any]], date: bool = True) -> List[Dict[str, Any]]:
+    """Функция принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание) возвращает новый список, отсортированный по дате."""
+    sorted_dates = sorted(user_dict, key=lambda x: x["date"], reverse=date)
+    return sorted_dates
